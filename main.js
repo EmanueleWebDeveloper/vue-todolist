91b09ae42151e35db9ea29fbde107ed7a0d5d765
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-29 21:20:18 +0100

    lineatura con input

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-const { createApp } = Vue
- 
-  createApp({
-    data() {
-      return {
-        newTask: '',
-        message: 'La mia lista della spesa!',
-        tasks: [
-            'fare i compiti',
-            'andare dal dottore',
-            'aggiustare la tv'
-        ]
-      }
-    },
+const { createApp } = Vue;
 
+createApp({
+    data() {
+        return {
+            message: "La tua Lista Attività",
+            newTask: "",
+            tasks: [
+                {
+                    text: 'fare i compiti',
+                    done: false,
+                },
+                {
+                    text: 'andare dal medico',
+                    done: false,
+                }
+            ]
+        };
+    },
     methods: {
         addTask() {
-            if (this.newTask !== '') {
-                this.tasks.push(this.newTask);
+            if (this.newTask!== '') {
+                this.tasks.unshift({ text: this.newTask, done: false });
             }
             this.newTask = '';
         },
         removeTask(index) {
             this.tasks.splice(index, 1);
+        },
+        toggleTaskStatus(index) {
+            this.tasks[index].done = !this.tasks[index].done;
         }
     }
 }).mount('#app');
