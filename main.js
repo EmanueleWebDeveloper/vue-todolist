ff14dd068a74f927bbfb73d0c6cfdda30c1d97a1
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-29 15:43:21 +0100

    base Html

---
+++ main.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue
+ 
+  createApp({
+    data() {
+      return {
+        message: 'La mia lista della spesa!',
+      }
+    },
+    
+    methods: {
+        
+    }
+  }).mount('#app')
