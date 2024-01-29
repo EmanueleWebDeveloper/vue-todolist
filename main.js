355b960aeacdd89a684ddbeff1fe2b449321a3d0
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-29 16:56:08 +0100

    keyUp, tasks con button

--- main.js
+++ main.js
@@ -3,11 +3,25 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
+        newTask: '',
         message: 'La mia lista della spesa!',
+        tasks: [
+            'fare i compiti',
+            'andare dal dottore',
+            'aggiustare la tv'
+        ]
       }
     },
-    
+
     methods: {
-        
+        addTask() {
+            if (this.newTask !== '') {
+                this.tasks.push(this.newTask);
+            }
+            this.newTask = '';
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
+        }
     }
-  }).mount('#app')
+}).mount('#app');
